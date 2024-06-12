41119dca4960e6d43dd8669acf19b06858d07f8a
souichirou <souichirou@DESKTOP-2NP9KMJ>
2024-06-13 00:24:30 +0900

    aa

--- src/main/java/com/example/batch/common/DataSourceConfiguration.java
+++ src/main/java/com/example/batch/common/DataSourceConfiguration.java
@@ -2,6 +2,9 @@ package com.example.batch.common;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.ExecutorType;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -14,11 +17,18 @@ public class DataSourceConfiguration {
 	@Bean
 	@ConfigurationProperties(prefix = "spring.datasource1")
 	public DataSource dataSource() {
-		return DataSourceBuilder.create().build();
+		DataSource d = DataSourceBuilder.create().build();
+		return d;
 	}
 	
 	@Bean
 	public JdbcTransactionManager transactionManager(DataSource dataSource) {
 		return new JdbcTransactionManager(dataSource);
+	}
+	
+	@Bean
+	public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+	    // Spring Batchと互換性のあるExecutorType.BATCHを設定
+	    return new SqlSessionTemplate(sqlSessionFactory, ExecutorType.BATCH);
 	}
 }
--- src/main/java/com/example/batch/processor/ItemProcessor01.java
+++ src/main/java/com/example/batch/processor/ItemProcessor01.java
@@ -3,27 +3,42 @@ package com.example.batch.processor;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.batch.core.ChunkListener;
 import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.ItemWriteListener;
 import org.springframework.batch.core.SkipListener;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.StepExecutionListener;
 import org.springframework.batch.core.StepListener;
+import org.springframework.batch.core.annotation.AfterWrite;
 import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.item.Chunk;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import com.example.batch.bean.ItemReaderBean01;
 import com.example.batch.bean.ItemWriterBean01;
 import com.example.batch.common.BaseProcessor;
+import com.example.batch.mapper.SampleMapper;
 
 @Component
 public class ItemProcessor01 extends BaseProcessor<ItemReaderBean01, ItemWriterBean01>
          implements SkipListener<ItemReaderBean01, ItemWriterBean01>
-                   , StepListener, ChunkListener, StepExecutionListener{
+                   , StepListener, ChunkListener, StepExecutionListener, ItemWriteListener<ItemWriterBean01>{
 	
 	private int chunknum = 0;
 	private int skipnum = 0;
 	private List<Throwable> exceptions = new ArrayList<>();
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Autowired
+	private SampleMapper sampleMapper;
 	
 	@Override
 	protected ItemWriterBean01 executeIndividually(ItemReaderBean01 item) {
@@ -31,6 +46,18 @@ public class ItemProcessor01 extends BaseProcessor<ItemReaderBean01, ItemWriterB
 		output.setTest(item.getTest());
 		output.setData(item.getData());
 		output.setId(item.getId());
+		ItemWriterBean01 output2 = new ItemWriterBean01();
+		int id = Integer.parseInt(item.getId())+200;
+		output2.setTest(item.getTest());
+		output2.setData(item.getData());
+		output2.setId(Integer.toString(id));
+		
+		sampleMapper.insert001(output2);
+		
+//		sqlSession.update("com.example.batch.mapper.SampleMapper.insert001", output2);
+		
+		
+		
 		return output;
 	}
 	
@@ -57,6 +84,11 @@ public class ItemProcessor01 extends BaseProcessor<ItemReaderBean01, ItemWriterB
 		chunknum++;
 		ChunkListener.super.beforeChunk(context);
 		System.out.println(chunknum + "チャンク開始");
+	}
+	@Override
+	public void afterWrite(Chunk<? extends ItemWriterBean01> items) {
+		// TODO 自動生成されたメソッド・スタブ
+		ItemWriteListener.super.afterWrite(items);
 	}
 
 }
